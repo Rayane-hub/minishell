6e973db87bb1a6218284e2e5f8f0ba4214ae482e
Rayane <114923586+Rayane-hub@users.noreply.github.com>
2024-05-16 18:08:54 +0200

    save 16mai heredoc free

--- fork.c
+++ fork.c
@@ -3,69 +3,172 @@
 /*                                                        :::      ::::::::   */
 /*   fork.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rasamad <rasamad@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: rasamad <rasamad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 15:12:43 by rasamad           #+#    #+#             */
-/*   Updated: 2024/04/05 11:16:09 by rasamad          ###   ########.fr       */
+/*   Updated: 2024/05/16 13:24:21 by rasamad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../includes/minishell.h"
 
-int	ft_first_fork(t_lst *elem, t_struct *var, char **envp)
+int	ft_fd_rand_heredoc(t_cmd *lst)
+{
+	char str_rand[6 + 1]; // +1 pour le caractère nul
+	char charset[] = "aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789";
+	int fd_rand;
+	int	i;
+
+	fd_rand = open("/dev/urandom", O_RDONLY);
+	if (fd_rand == -1) 
+	{
+		perror("Erreur lors de l'ouverture de /dev/urandom");
+		return 1;
+	}
+	if (read(fd_rand, str_rand, 6) == -1)//read dans str_rand 5 caractere de dev/urandom
+	{
+		perror("Erreur lors de la lecture de /dev/urandom");
+		close(fd_rand);
+		return 1;
+	}
+	close(fd_rand);
+	//str_rand[0] = '.';
+	i = 0;
+	while (i < 6)
+	{
+		str_rand[i] = charset[str_rand[i] % (sizeof(charset) - 1)];
+		i++;
+	}
+	str_rand[6] = '\0';
+	lst->fd_str_rand = open(str_rand, O_CREAT | O_RDWR, 0777);//open du fichier avec un nom random
+	if (lst->fd_str_rand == -1) {
+		perror("Erreur lors de la création du fichier");
+		return 1;
+	}
+	i = 0;
+	while (lst->heredoc_content[i])
+	{
+		write(lst->fd_str_rand, lst->heredoc_content[i], ft_strlen(lst->heredoc_content[i]));
+		write(lst->fd_str_rand, "\n", 1);
+		i++;
+	}
+	return 0;
+}
+
+int	ft_fd_heredoc(t_cmd *lst)
+{
+	int	i;
+
+	lst->fd_str_rand = open(".heredoc", O_CREAT | O_RDWR | O_TRUNC, 0777);//open du fichier avec un nom random
+	if (lst->fd_str_rand == -1) 
+	{
+		perror("Erreur lors de la création du fichier");
+		return -1;
+	}
+	i = 0;
+	while (lst->heredoc_content[i])
+	{
+		write(lst->fd_str_rand, lst->heredoc_content[i], ft_strlen(lst->heredoc_content[i]));
+		write(lst->fd_str_rand, "\n", 1);
+		i++;
+	}
+	close(lst->fd_str_rand);
+	return 0;
+}
+
+
+/*******************************************************************************************************/
+
+int	ft_first_fork(t_data *data)
 {
 	pid_t	pid;
+	t_cmd	*lst;
 
 	pid = fork();
-	if (pid < 0){
-		perror("fork first failed :");	
-		exit(1);
-	}
-	if (pid == 0)
+	if (pid < 0)
 	{
-		//prend dans fd_infile en prioriter sinon dans stdin
-		if (elem->redirection && elem->fd_infile > 0)// < f1
-		{
-			if (dup2(elem->fd_infile, STDIN_FILENO) == -1)
+		perror("fork first failed :");	
+		exit(EXIT_FAILURE);
+	}
+	if (pid == 0) //Children
+	{
+		lst = data->cmd;
+		//1. SI il y a un infile ET quil est en dernier  sil y en a un et quil est en dernier
+		if (lst->redirecter && lst->fd_infile > 0 && lst->end_heredoc == 0)// < f1
+		{//recupere les donner dans fd_infile au lieu de lentree standard
+			if (dup2(lst->fd_infile, STDIN_FILENO) == -1)
 			{
 				perror("dup2 first fd_infile failed : ");
 				exit(EXIT_FAILURE);
 			}
 		}
-		//envoi dans fd_outfile en prioriter s'il y en a un
-		if (elem->redirection && elem->fd_outfile > 0)// > f2
+		//2. SINON SI il y a un heredoc et quil est en dernier 
+		else if (lst->heredoc_content && lst->end_heredoc == 1)// << eof
+		{//appelle ft qui cp heredoc_content dans un fichier temporaire
+			if (ft_fd_heredoc(lst) == -1)
+				exit(EXIT_FAILURE);
+			lst->fd_str_rand = open(".heredoc", O_RDONLY);
+			if (dup2(lst->fd_str_rand, STDIN_FILENO) == -1)	//puis dup ce fd,  sinon dans stdin
+			{
+				perror("dup2 first fd_str_rand failed : ");
+				exit(EXIT_FAILURE);
+			}
+			close(lst->fd_str_rand); // Fermer le descripteur de fichier heredoc
+		}
+		//1. SI OUTPUT envoi dans fd_outfile en prioriter
+		if (lst->redirecter && lst->fd_outfile > 0)// > f2 || >> f2
 		{
-			if (dup2(elem->fd_outfile, STDOUT_FILENO) == -1)
+			if (dup2(lst->fd_outfile, STDOUT_FILENO) == -1)
 			{
 				perror("dup2 first fd_outfile failed : ");
 				exit(EXIT_FAILURE);
 			}
 		}
-		//sinon dans pipe[1] si il y a un next, sinon dans stdout
-		else if (elem->next) // --> | cmd
+		//2. SINON SI Y A UN NEXT envoi dans pipe[1], sinon dans stdout
+		else if (lst->next) // --> | cmd
 		{
-			if (dup2(var->pipe_fd[1], STDOUT_FILENO) == -1)
+			if (dup2(data->pipe_fd[1], STDOUT_FILENO) == -1)
 			{
 				perror("dup2 first pipe[1] failed : ");
 				exit(EXIT_FAILURE);
 			}
 		}
-		execve(elem->path_cmd, elem->args, envp);
+		if (ft_builtins(lst) != 0)
+			exit(0);
+		if (data->exit_code != 0)
+			exit(EXIT_SUCCESS);
+		execve(lst->path_cmd, lst->args, data->var.mini_env);
 		perror("execve 1 : failed ");
-		exit(0);
+		exit(EXIT_FAILURE);//comment retourner le bon code d'erreur en fonction du cas d'erreur ? Grace a la macro de waitpid WIFEXITED
 	}
 	else if (pid > 0)
 	{
-		int	status;
-		wait(&status);ft_printf("status de l'enfant first %d\n", status);
-		//ft_printf("je suis dans le processus du parent du 1er fork, le pid de l'enfant est %d\n", pid);
+		int status;
+    	waitpid(pid, &status, 0);
+		
+		if (status == 0)
+			return (-1);
+		if (WIFEXITED(status)) {//execve failed
+			printf("Command 1st failed with exit status: %d\n", WIFEXITED(status));
+			// Ici, vous pouvez ajuster votre logique en fonction de exit_status
+			exit_status(data, WIFEXITED(status), "");//Pourquoi une fois echo $? apres cmd not perm il affiche pas exit_stat qui semble etre bien a 1, cest pcq je fait pas return -1`
+			return (-1);
+		}
+		if (WIFSIGNALED(status)) {
+			int signal_number = WTERMSIG(status);
+			printf("Command terminated by signal: %d\n", signal_number);
+			// Ici, vous pouvez ajuster votre logique en fonction du signal reçu
+		}
 	}
 	return (0);
 }
 
-int	ft_middle_fork(t_lst *elem, t_struct *var, char **envp)
+/******************************************************************************************************/
+
+int	ft_middle_fork(t_data *data)
 {
 	pid_t	pid;
+	t_cmd *lst;
 
 	pid = fork();
 	if (pid < 0)
@@ -75,105 +178,169 @@ int	ft_middle_fork(t_lst *elem, t_struct *var, char **envp)
 	}
 	else if (pid == 0)
 	{
-		//prend soit dans fd_infile en prioriter sil y en a un
-		if (elem->redirection && elem->fd_infile > 0)
-		{
-			ft_printf("IN0\n");
-			if (dup2(elem->fd_infile, STDIN_FILENO))
+		lst = data->cmd;
+		//1. SI il y a un infile ET quil est en dernier  sil y en a un et quil est en dernier
+		if (lst->redirecter && lst->fd_infile > 0 && lst->end_heredoc == 0)// < f1
+		{//recupere les donner dans fd_infile au lieu de lentree standard
+			if (dup2(lst->fd_infile, STDIN_FILENO) == -1)
 			{
 				perror("dup2 middle fd_indile failed :");
 				exit(EXIT_FAILURE);
 			}
 		}
-		//soit dans pipe[0]
-		else
-		{
-			if (dup2(var->save_pipe, STDIN_FILENO))
+		//2. SINON SI il y a un heredoc et est en dernier
+		else if (lst->heredoc_content && lst->end_heredoc == 1)// << eof
+		{//appelle ft qui cp heredoc_content dans un fichier temporaire
+			if (ft_fd_heredoc(lst) == -1)
+				exit(EXIT_FAILURE);
+			lst->fd_str_rand = open(".heredoc", O_RDONLY);
+			//puis recupere les donne du fd .heredoc au lieu de lentree standard
+			if (dup2(lst->fd_str_rand, STDIN_FILENO) == -1)	//puis dup ce fd,  sinon dans stdin
+			{
+				perror("dup2 first fd_str_rand failed : ");
+				exit(EXIT_FAILURE);
+			}
+			close(lst->fd_str_rand);
+		}
+		//3. SINON SI save pipe contient bien data pipe[0]
+		else if (data->save_pipe)
+		{//recupere les donnes dans save_pipe au lieu de lentree standard
+			if (dup2(data->save_pipe, STDIN_FILENO) == -1)
 			{
 				perror("dup2 middle pipe_fd[0] failed :");
 				exit(EXIT_FAILURE);
 			}
 		}
-		//envoi dans fd_outfile en prioriter sil y en a un
-		if (elem->redirection && elem->fd_outfile > 0)
-		{
-			if (dup2(elem->fd_outfile, STDOUT_FILENO))
+		//1. SI redirecteur != NULL et que fd_outfile a de la data
+		if (lst->redirecter && lst->fd_outfile > 0)
+		{//met la data de la sortie de lexecve dans fd_outfile
+			if (dup2(lst->fd_outfile, STDOUT_FILENO) == -1)
 			{
 				perror("dup2 middle fd_outfile failed :");
 				exit(EXIT_FAILURE);
 			}
 		}
-		//sinon dans pipe[1]
-		else if (elem->next)
-		{
-			ft_printf("PI1\n");
-			if (dup2(var->pipe_fd[1], STDOUT_FILENO) == -1)
+		//2. SINON SI il y a bien un next
+		if (lst->next)
+		{//met la data de la sortie de lexecve dans lextremiter decriture du pipe
+			if (dup2(data->pipe_fd[1], STDOUT_FILENO) == -1)
 			{
 				perror("dup2 middle pipe[1] failed :");
 				exit(EXIT_FAILURE);
 			}
 		}
-		execve(elem->path_cmd, elem->args, envp);
+		if (ft_builtins(lst) != 0)
+			exit(0);
+		if (data->exit_code != 0)
+			exit(EXIT_SUCCESS);
+		execve(lst->path_cmd, lst->args, data->var.mini_env);
 		perror("execve 2 failed : ");
-		//free elem->path et args
-		exit(0);
+		//free lst->path et args
+		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		int	status;
-		wait(&status);ft_printf("status de l'enfant midlle %d\n", status);
+		int status;
+    	waitpid(pid, &status, 0);
+
+		if (status == 0)
+			return (-1);
+		if (WIFEXITED(status)) {//execve failed
+			printf("Command mid failed with exit status: %d\n", WIFEXITED(status));
+			// Ici, vous pouvez ajuster votre logique en fonction de exit_status
+			exit_status(data, WIFEXITED(status), "");//Pourquoi une fois echo $? apres cmd not perm il affiche pas exit_stat qui semble etre bien a 1, cest pcq je fait pas return -1`
+			return (-1);
+		}
+		if (WIFSIGNALED(status)) {
+			int signal_number = WTERMSIG(status);
+			printf("Command terminated by signal: %d\n", signal_number);
+			// Ici, vous pouvez ajuster votre logique en fonction du signal reçu
+		}
 	}
 	return (0);
 }
 
-int	ft_last_fork(t_lst *elem, t_struct *var, char **envp)
+/********************************************************************************************************/
+
+int	ft_last_fork(t_data *data)
 {
 	pid_t	pid;
+	t_cmd	*lst;
 
 	pid = fork();
 	if (pid < 0){
 		perror("fork last failed :");	
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 	if (pid == 0)
 	{
-		//prend soit dans infile en prioriter sil y en a un
-		if (elem->redirection && elem->fd_infile > 0)// < f1
+		lst = data->cmd;
+		//1. prend soit dans infile en prioriter sil y en a un
+		if (lst->redirecter && lst->fd_infile > 0 && lst->end_heredoc == 0)// < f1
 		{
-			if (dup2(elem->fd_infile, STDIN_FILENO) == -1)
+			if (dup2(lst->fd_infile, STDIN_FILENO) == -1)
 			{
 				perror("dup2 last fd_infile failed : ");
 				exit(EXIT_FAILURE);
 			}
 		}
-		//soit dans pipe[0]
-		else
+		//2. SINON SI HEREDOC  
+		else if (lst->heredoc_content && lst->end_heredoc == 1)// << eof
 		{
-			if (dup2(var->save_pipe, STDIN_FILENO) == -1) // sortie de l'ancienne cmd
+			if (ft_fd_heredoc(lst) == -1)	//apelle ft qui va open un fd et cp le heredoc dedans
+				exit(EXIT_FAILURE);
+			lst->fd_str_rand = open(".heredoc", O_RDONLY);
+			if (dup2(lst->fd_str_rand, STDIN_FILENO) == -1)	//puis dup ce fd,  sinon dans stdin
+			{
+				perror("dup2 first fd_str_rand failed : ");
+				exit(EXIT_FAILURE);
+			}
+			close(lst->fd_str_rand);
+		}
+		//3. soit dans pipe[0]
+		else if (data->save_pipe)
+		{
+			if (dup2(data->save_pipe, STDIN_FILENO) == -1) // sortie de l'ancienne cmd
 			{
 				perror("dup2 last pipe_fd[0] failed : ");
 				exit(EXIT_FAILURE);
 			}
 		}
-		//envoi soit dans fd_outfile s'il y en a un soit dans stdout qui reste inchange (pas de dup2)
-		if (elem->redirection && elem->fd_outfile > 0)// > f2
+		//1. envoi soit dans fd_outfile s'il y en a un soit dans stdout qui reste inchange (pas de dup2)
+		if (lst->redirecter && lst->fd_outfile > 0)// > f2
 		{
-			if (dup2(elem->fd_outfile, STDOUT_FILENO) == -1)
+			if (dup2(lst->fd_outfile, STDOUT_FILENO) == -1)
 			{
 				perror("dup2 last fd_outfile failed : ");
 				exit(EXIT_FAILURE);
 			}
 		}
-		execve(elem->path_cmd, elem->args, envp);
+		if (ft_builtins(lst) != 0)
+			exit(0);
+		if (data->exit_code != 0)
+			exit(EXIT_SUCCESS);
+		execve(lst->path_cmd, lst->args, data->var.mini_env);
 		perror("execve 3 : failed ");
-		exit(0);
-	}
+		exit(EXIT_FAILURE);
+	} 
 	else if (pid > 0)
 	{
-		int	status;
-		wait(&status);ft_printf("status de l'enfant last %d\n", status);
-		//ft_printf("je suis dans le processus du parent du dernier fork, le pid de l'enfant est %d\n", pid);
-		close(var->pipe_fd[0]);
+		int status;
+    	waitpid(pid, &status, 0);
+		
+		if (status == 0)// ne pas changer exit_code soit pcq il a deja etais modif en amont avec une err soit pcq tout est good
+			return (-1);
+		if (WIFEXITED(status)) {//execve failed
+			printf("Command last failed with exit status: %d\n", WIFEXITED(status));
+			// Ici, vous pouvez ajuster votre logique en fonction de exit_status
+			exit_status(data, WIFEXITED(status), "");//Pourquoi une fois echo $? apres cmd not perm il affiche pas exit_stat qui semble etre bien a 1, cest pcq je fait pas return -1`
+			return (-1);
+		}
+		if (WIFSIGNALED(status)) {
+			int signal_number = WTERMSIG(status);
+			printf("Command terminated by signal: %d\n", signal_number);
+			// Ici, vous pouvez ajuster votre logique en fonction du signal reçu
+		}
 	}
 	return (0);
 }
--- minishell.c
+++ minishell.c
@@ -6,225 +6,146 @@
 /*   By: rasamad <rasamad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 11:02:31 by jgavairo          #+#    #+#             */
-/*   Updated: 2024/04/30 17:44:54 by rasamad          ###   ########.fr       */
+/*   Updated: 2024/05/16 17:27:04 by rasamad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
 
-#include <stdlib.h>
-#include <string.h>
-
-char **ft_realloc(char *rl, t_cmd **lst)
+void	ft_unset(t_env **mini_env, t_cmd *cmd)
 {
-	size_t		i;
-	char	**tab;
-
-	i = 0;
-	while ((*lst)->heredoc_content[i])
-		i++;
-	if (i == 0)
-		tab = malloc(sizeof(char*) * i + 2);
-	else
-		tab = malloc(sizeof(char*) * i + 1);
-	i = 0;
-	while ((*lst)->heredoc_content[i])
-	{
-		tab[i] = ft_strdup((*lst)->heredoc_content[i]);
-		i++;
-	}
-	tab[i] = ft_strdup(rl);
-	return (tab);
-}
-
-int ft_heredoc(t_cmd *lst) {
-    int i = 0;  // Indice du nombre de Heredocs
-    char *line;
-
-    //lst->heredoc_content = malloc(sizeof(char *) * 1);  // Alloue de la mémoire initialement
-    //lst->heredoc_content = NULL;
-	t_cmd *begin = lst;
-	
-	while (lst){
-		while (i < lst->nb_del) 
-		{
-			line = readline(">");
-			if (!line)
-			{
-				perror("readline failed");
-				return -1;
-			}
-			while (ft_strncmp(line, lst->delimiter[i], ft_strlen(line)) != 0)
-			{
-				lst->heredoc_content = ft_realloc(line, &lst);
-				free(line);  // Libère la mémoire allouée par readline
-				line = readline(">");
-				if (!line)
-				{
-					perror("readline failed");
-					return -1;
-				}
-			}
-			i++;
-			//lst->heredoc_content[j] = NULL;
-		}
-		i = 0;
-		lst = lst->next;
-	}
-    // Affiche le dernier Heredoc stocké
-	i++;
-	int j = 0;
-	while (begin != NULL)
-	{
-		printf("heredoc %d :\n", i);
-		i++;
-		while (begin->heredoc_content[j]){
-			printf("%s\n", begin->heredoc_content[j]);
-			j++;
-		}
-		j = 0;
-		begin = begin->next;
-	}
-
-    // Nettoyage complet
-    /*for (int k = 0; k < j; k++) {
-        free(lst->heredoc_content[k]);  // Libère chaque chaîne
-    }
-    free(lst->heredoc_content);  // Libère le tableau lui-même*/
-    return 0;
-}
-
-int	launch_exec(t_cmd **lst, char **envp)
-{
-    t_struct   	*var;
-    int        	i;
-
-	var = malloc(sizeof(t_struct));
-	var->pipe_fd[0] = 0;
-	var->pipe_fd[1] = 0;
-	var->save_pipe = 0;
-    i = 0;
-    int len_lst = ft_lstlen((*lst));
-    while (*lst)
-    {
-        i++;
-        (*lst)->open = 0;
-        if ((*lst)->redirecter)        //2. redirecter check
-            ft_redirecter(*lst);
-        if ((*lst)->next != NULL)        //3. Pipe check ne peut etre fait si 3 ou plus de cmd car il va refaire un pipe et erase lancien alors aue pour 2 cmd il fait qun pipe 
-        {
-            if (pipe(var->pipe_fd) == -1)
-			{
-                perror("pipe failed");
-                exit(EXIT_FAILURE);
-            }
-        }
-		if ((*lst)->heredoc == true)
-		{
-			ft_heredoc(*lst);
-			exit(0);
-		}
-		//cas ou la partie suivante ne doit pas etre faite, heredoc sans cmd, builtings
-        ft_check_access((*lst), envp);    //4 Cmd check
-
-        if (i == 1 && (*lst)->open == 0)
-		{   //5. exec (cmd_first) | cmd_middle... | cmd_last
-            //printf("go exec first cmd\n\n");
-            ft_first_fork((*lst), &var, envp);
-			if (var->pipe_fd[1] > 3)
-            	close(var->pipe_fd[1]);// je close lecriture pour pour pas que la lecture attendent indefinement.
-            var->save_pipe = var->pipe_fd[0]; //je save la lecture pour le next car je vais re pipe pour avoir un nouveau canal 
-        }
-
-        else if (i < len_lst && (*lst)->open == 0)
-		{	//6. exec cmd_first | (cmd_middle...) | cmd_last
-            //printf("go exec middle cmd\n\n");
-            ft_middle_fork((*lst), &var, envp);
-            close(var->pipe_fd[1]);
-            var->save_pipe = var->pipe_fd[0];
-        }
-
-        else if (i == len_lst && (*lst)->open == 0)
-		{	//7. exec  exec cmd_first | cmd_middle... | (cmd_last)
-            //printf("go exec last cmd\n\n");
-            ft_last_fork((*lst), &var, envp);
-            close(var->pipe_fd[0]);
-        }
-        //ft_free_token(lst);
-        ft_close(*lst);
-        (*lst) = (*lst)->next;
-    }
-	free(var);
-    return (0);
-}
-
-int main(int argc, char **argv, char **envp)
-{
-	char	*rl;
-	char	*pwd;
-	char	**input;
-	char	**pipes;
+	t_env	*tmp;
+	t_env	*swap;
 	int		i;
-	t_var	*var;
-	t_cmd	*cmd = NULL;
-	t_cmd	*tmp = NULL;
+
+	i = 1;
+	tmp = (*mini_env);
+	while (cmd->args[i])
+	{
+		while (tmp->next && ft_strcmp(tmp->next->name, cmd->args[i]) != 0)
+			tmp = tmp->next;
+		if (tmp && ft_strcmp(tmp->next->name, cmd->args[i]) == 0)
+		{
+			swap = tmp->next;
+			tmp->next = tmp->next->next;
+			free(swap->name);
+			free(swap->value);
+			free(swap);
+		}
+		i++;
+	}
+}
+
+int	ft_builtins_env(t_cmd *cmd, t_env *mini_env)
+{
+	if (ft_strcmp(cmd->args[0], "export") == 0)
+	{
+		ft_export(&mini_env, cmd);
+			return (1);
+	}
+	else if (ft_strcmp(cmd->args[0], "unset") == 0)
+	{
+		ft_unset(&mini_env, cmd);
+		return (1);
+	}
+	else if (ft_strcmp(cmd->args[0], "env") == 0)
+	{
+		env_cmd(mini_env);
+		return (1);
+	}
+	return (0);
+}
+
+int	launch_exec(t_data *data)
+{
+    int        	i;
+	t_data	*begin;
+
+	begin = data;
+	data->var.mini_env = ft_list_to_tab(data->mini_env);
+	if (!data->var.mini_env)
+		return (-1);
+	data->save_pipe = 0;
+    i = 0;
+	int len_lst = ft_lstlen(data->cmd);
+	if (ft_heredoc(data) == -1 || !data->cmd->args[0])
+		return (ft_free_all_heredoc(begin), -1);
+	//ft_display_heredoc(data->cmd);
+	while (data->cmd)
+	{
+		data->exit_code = 0;
+		i++;
+		if (data->cmd->redirecter)	//2. redirecter check
+		ft_redirecter(data);
+		if (data->cmd->next != NULL)		//3. Pipe check ne peut etre fait si 3 ou plus de cmd car il va refaire un pipe et erase lancien alors aue pour 2 cmd il fait qun pipe
+			if (pipe(data->pipe_fd) == -1)
+				exit_status(data, 1, "pipe failed\n");
+		//cas ou la partie suivante ne doit pas etre faite, heredoc sans cmd, builtings
+		if (ft_builtins_env(data->cmd, data->mini_env) == 0)
+		{
+			int check_access = ft_check_access(data);
+			if (check_access == -1)  //4 Cmd check
+				exit_status(data, 127, "");
+			else if (check_access == -2)  //4 Cmd check
+				exit_status(data, 127, "malloc error from [ft_check_access]");
+			if (i == 1)
+			{	//5. exec (cmd_first) | cmd_middle... | cmd_last
+				if (ft_first_fork(data) == -1)
+				if (data->pipe_fd[1] > 3)
+					close(data->pipe_fd[1]);// je close lecriture pour pas que la lecture attende indefinement.
+				data->save_pipe = data->pipe_fd[0]; //je save la lecture pour le next car je vais re pipe pour avoir un nouveau canal 
+			}
+			else if (i < len_lst)
+			{	//6. exec cmd_first | (cmd_middle...) | cmd_last
+				ft_middle_fork(data);
+				close(data->pipe_fd[1]);
+				data->save_pipe = data->pipe_fd[0];
+			}
+			else if (i == len_lst)
+			{	//7. exec  exec cmd_first | cmd_middle... | (cmd_last)
+				ft_last_fork(data);
+				close(data->pipe_fd[0]);
+			}
+		}
+		ft_close(data->cmd);
+		data->cmd = data->cmd->next;
+	}
+	ft_free_all_heredoc(begin);
+	if (data->exit_code != 0)
+		return (-1);
+	return (0);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	int		i;
+	t_data	data;
+
 	(void)argc;
 	(void)argv;
-	var = malloc(sizeof(t_var));
-	if (!var)
-		printf("t_var memory allocation Error\n");
-	var->mini_env = NULL;
-	pipes = NULL;
-	rl = NULL;
 	i = 0;
-	//printf_title();
-	while(1)
+	data.exit_code = 0;
+	if (minishell_starter(envp, &data) == -1)
+		return (printf("Malloc error\n"), -1);
+	while (1)
 	{
-		pwd = getcwd(NULL, 0);	//je recupere le chemin d'acces pour l'afficher tel fish
-		//var->mini_env = env_copyer(envp, var); // je recupere et copie lenvironnement dans un autre tableau
-		printf ("\033[90m%s\033[0m", pwd);	//je l'affiche
-		rl = readline("\e[33m$> \e[37m");	//je recupere la ligne en entree dans une boucle infini afin de l'attendre
-		if (syntaxe_error(rl) == 0)	//je check les erreurs de syntaxes et lance le programme seulement si tout est OK
+		if (prompt_customer(&data) == 0)
 		{
-			negative_checker(rl);		//je check toutes mes cotes et passe en negatif tout ce qui ne vas pas devoir etre interprete
-			rl = dolls_expander(rl);
-			//if (pipes_counter(rl) == 1)
-			pipes = ft_split(rl, '|');	//je separe chaque commande grace au pipe
-			// else
-			// {
-			// 	pipes = malloc(sizeof(char *) * 2);
-			// 	pipes[0] = ft_strdup(rl);
-			// 	pipes[1] = NULL;
-			// }
-			add_history(rl);
-			free(rl);
-			while (pipes[i])	//chaque commande est rangee par ligne dans un tableau afin de les traiter une a une 
+			if (data.var.rl[0] != '\0' && syntaxe_error(&data, data.var.rl) == 0)
 			{
-				tmp = ft_lstnew_minishell(); //jinitialise ma structure car nous allons en avoir besoin
-				if (!tmp)
-					break;
-				if (heredoc_checker(pipes[i], &tmp) == -1)
-					printf("Heredoc memory error\n");
-				redirecter(pipes[i], &tmp);	//je vais chercher toutes les redirects afin de les ranger dans une variable a part
-				pipes[i] = redirect_deleter(pipes[i]);	//je vai supprimer toutes les redirects de la ligne principale afin de ne plus les prendre en compte
-				input = ft_split(pipes[i], ' ');	//il me reste donc plus que la commande et les arguments separes d'espaces, je les separe donc et les range dans input
-				if (stock_input(input, &tmp) == 0)	//je vais recuperer le nom de la commande et les arguments dans input et les ranger dans la structure tmp
-				{
-					i++;	// la commande a ete traitee, je passe a la prochaine (si il y en a une)
-					ft_lstadd_back_minishell(&cmd, tmp);	//j'ajoute le noeud tmp ou tout a ete ranger a ma liste chainee 
-				}
+				if (parser(&data) == 0)
+					if (final_parse(&data) == -1)
+					{
+						ft_lstclear(&data.cmd);
+						free_pipes(data.var.pipes);
+						free(data.var.pwd);
+					}
 			}
-			i = 0;
-			ft_printf_struct(cmd);
-			//start = cmd;
-			printf("\033[38;5;220mLancement de Launch_exec...\033[0m\n");
-			if (launch_exec(&cmd, envp) == -1)
-				printf ("Error exec\n");
-			printf("--------------------------------------------------------\n");
-			ft_lstclear(&cmd);
-			free_pipes(pipes);
-			//free(pwd);
 		}
 	}
 	rl_clear_history();
 }
+
+//launch_exec | bultins
+
+//faire < (syntaxe err) puis ls
